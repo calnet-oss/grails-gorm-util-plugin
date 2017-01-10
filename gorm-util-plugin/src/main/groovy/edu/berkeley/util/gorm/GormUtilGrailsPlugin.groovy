293324a9352c0d800a7546daec2ce22b740dcacb
Søren Berg Glasius <soeren@glasius.dk>
2017-01-10 12:14:12 +0100

    CNR-1281 Migrated to Grails 3.x

---
+++ gorm-util-plugin/src/main/groovy/edu/berkeley/util/gorm/GormUtilGrailsPlugin.groovy
@@ -0,0 +1,66 @@
+package edu.berkeley.util.gorm
+
+import grails.plugins.Plugin
+
+/*
+ * Copyright (c) 2016, Regents of the University of California and
+ * contributors.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+class GormUtilGrailsPlugin extends Plugin {
+    // the version or versions of Grails the plugin is designed for
+    def grailsVersion = "3.0 > *"
+    // resources that are excluded from plugin packaging
+    def pluginExcludes = [
+    ]
+
+    def title = "Gorm Util Plugin" // Headline display name of the plugin
+    def author = "Brian Koehmstedt"
+    def authorEmail = "bkoehmstedt@berkeley.edu"
+    def description = '''\
+Grails GORM Utility plugin.
+'''
+
+    // URL to the plugin's documentation
+    def documentation = "https://github.com/calnet-oss/grails-gorm-util-plugin/"
+
+    // Extra (optional) plugin metadata
+
+    // License: one of 'APACHE', 'GPL2', 'GPL3'
+    def license = "BSD"
+
+    // Details of company behind the plugin (if there is one)
+    def organization = [ name: "University of California, Berkeley", url: "http://www.berkeley.edu/" ]
+
+    // Any additional developers beyond the author specified above.
+//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
+
+    // Location of the plugin's issue tracker.
+    def issueManagement = [ system: "GitHub", url: "https://github.com/calnet-oss/grails-gorm-util-plugin/issues" ]
+
+    // Online location of the plugin's browseable source code.
+    def scm = [ url: "https://github.com/calnet-oss/grails-gorm-util-plugin" ]
+
+}
